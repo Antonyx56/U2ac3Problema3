24337a26bdb15f3c12f77f1098b37ed6f1b64b35
antony <antonioseguracastillo96@gmail.com>
2026-03-19 08:00:59 -0600

    Update

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
+import java.util.ArrayList;
 public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private double cargaMaxima;
+    private String fechaAdquisicion;
+
+    private ArrayList<Viaje> viajes;
+
+    public Vehiculo(String marca, String modelo, double cargaMaxima, String fechaAdquisicion, ArrayList<Viaje> viajes) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cargaMaxima = cargaMaxima;
+        this.fechaAdquisicion = fechaAdquisicion;
+        this.viajes = viajes;
+    }
+    public void agregarViaje(Viaje v){
+        viajes.add(v);
+    }
+
+    public void cancelarViaje(Viaje v){
+        viajes.remove(v);
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", cargaMaxima=" + cargaMaxima +
+                ", fechaAdquisicion='" + fechaAdquisicion + '\'' +
+                '}';
+    }
 }
--- src/Viaje.java
+++ src/Viaje.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class Viaje {
+    private String ciudadDestino;
+    private String direccion;
+    private String fechaViaje;
+    private String fechaRegreso;
+    private String descripcionCarga;
+    private double monto;
+
+    public Viaje(String ciudadDestino, String direccion, String fechaViaje, String fechaRegreso, String descripcionCarga, double monto) {
+        this.ciudadDestino = ciudadDestino;
+        this.direccion = direccion;
+        this.fechaViaje = fechaViaje;
+        this.fechaRegreso = fechaRegreso;
+        this.descripcionCarga = descripcionCarga;
+        this.monto = monto;
+    }
+
+    public String getCiudadDestino() {
+        return ciudadDestino;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    @Override
+    public String toString() {
+        return "Viaje{" +
+                "ciudadDestino='" + ciudadDestino + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", fechaViaje='" + fechaViaje + '\'' +
+                ", fechaRegreso='" + fechaRegreso + '\'' +
+                ", descripcionCarga='" + descripcionCarga + '\'' +
+                ", monto=" + monto +
+                '}';
+    }
 }
